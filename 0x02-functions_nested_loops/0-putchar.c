bfc196ef7a98bf7a69ee259fe4662132b09a9ad7
Lanciline <salaheddinehamzi@gmail..com>
2023-03-21 13:41:49 -0700

    woooooooooork pls

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
 
 	int main(void)
@@ -21,9 +20,3 @@
 	return (0);
 	}
 
-=======
-#include<stdio.h>
-int main (void){
-	printf("_putchar /n");
-	return0;
->>>>>>> af3bf74eda74fbc16b97d90c60f7e1c142ee1c61
